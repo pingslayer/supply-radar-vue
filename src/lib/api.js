04edc846d81190df2dd6ec59762968d9fef94bac
pingslayer <ronaldprajval7@gmail.com>
2026-03-27 12:02:27 +0530

    env for api url

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://supplyradar.local/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
